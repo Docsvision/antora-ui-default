66fc8e259b582bbbe12ad4f066e4e8369c772ffb
Ivan Kutepov <kutepov.i@docsvision.com>
2022-05-11 10:00:18 +0300

    replace half broken font regexp

--- gulp.d/tasks/build.js
+++ gulp.d/tasks/build.js
@@ -37,7 +37,7 @@ module.exports = (src, dest, preview) => () => {
       }),
     postcssUrl([
       {
-        filter: new RegExp('^src/css/[~][^/]*(?:font|face)[^/]*/.*/files/.+[.](?:ttf|woff2?)$'),
+        filter: '**/~@fontsource/*/files/*',
         url: (asset) => {
           const relpath = asset.pathname.substr(1)
           const abspath = require.resolve(relpath)
