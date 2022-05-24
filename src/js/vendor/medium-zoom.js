ec7cf7e73d76bcc341b126d25705328a299e3b86
Markiev Vladimir <markiev.v@docsvision.com>
2022-05-24 11:02:35 +0300

    edit mzm

--- src/js/vendor/medium-zoom.js
+++ src/js/vendor/medium-zoom.js
@@ -1,4 +1,4 @@
 ;(function () {
   var Mzm = (window.mzm = require('medium-zoom/dist/medium-zoom.min'))
-  Mzm('span.frame img, span.zoom img', { background: '#fff' })
+  Mzm('span.image img, div.imageblock img', { background: '#fff', margin: 10 })
 })()
